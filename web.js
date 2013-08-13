0cb2837aa7c534cc202628c67524da43a8964a98
Panayotis Trapatsas <trapatsas@gmail.com>
2013-08-13 21:32:52 +0300

    web.js last change to silence deprecated warning

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var htmlfile = "index.html";
 
-var app = express.createServer(express.logger());
+var app = express();
 
 app.get('/', function(request, response) {
   var html = fs.readFileSync(htmlfile).toString();
