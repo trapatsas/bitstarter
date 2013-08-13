77f8eb66c271ff66ebb5c8eb3d2ce857121ae6fd
Panayotis Trapatsas <trapatsas@gmail.com>
2013-08-13 22:13:56 +0300

    delete old project

--- web.js
+++ web.js
@@ -1,14 +1,15 @@
 var express = require('express');
-var app = express.createServer(express.logger());
+var fs = require('fs');
+var htmlfile = "index.html";
+
+var app = express();
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var buf = new Buffer(fs.readFileSync('index.html'), 'utf-8');
-    response.send(buf.toString());
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
